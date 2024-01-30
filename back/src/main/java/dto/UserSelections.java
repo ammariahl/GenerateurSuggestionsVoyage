491c61c028d48d00265f62055f163ece83ea29bc
MalPro <malshis@yahoo.com>
2024-01-30 13:55:24 +0100

    add identify to the userSelections component and getters and setters

--- back/src/main/java/dto/UserSelections.java
+++ back/src/main/java/dto/UserSelections.java
@@ -1,9 +1,55 @@
 package com.whereto.destination.dto;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserSelections {
     private String season;
     private String budget;
     private String weather;
     private String activity;
     private String document;
+
+public String getSeason() {
+        return season;
+    }
+
+    public void setSeason(String season) {
+        this.season = season;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getWeather() {
+        return weather;
+    }
+
+    public void setWeather(String weather) {
+        this.weather = weather;
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+
+
 }
