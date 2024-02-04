a074b925c707e1555525c17e855d0737aea366ec
AzraeLandres <azraellandres@gmail.com>
2024-02-04 14:31:56 +0100

    add components, add routes, side image

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeComponent } from './home/home.component';
 import { NgOptimizedImage } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SuggestionComponent } from './suggestion/suggestion.component';
 import { SearchDestinationComponent } from './search-destination/search-destination.component';
+import { DestinationComponent } from './destination/destination.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, SuggestionComponent, SearchDestinationComponent],
-  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage, MenuComponent],
+  declarations: [
+    AppComponent,
+    SuggestionComponent,
+    SearchDestinationComponent,
+    DestinationComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
 })
