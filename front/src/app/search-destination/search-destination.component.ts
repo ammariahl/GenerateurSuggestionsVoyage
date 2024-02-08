c5525d49cf1acc4131b30d531601d2a083e37473
ammariahl <ammariahellou@gmail.com>
2024-02-08 10:03:50 +0100

    Update search and suggestion component about their disposition

--- front/src/app/search-destination/search-destination.component.ts
+++ front/src/app/search-destination/search-destination.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DestinationCriteria } from '../models/destination-criteria.model';
 
 @Component({
   selector: 'app-search-destination',
