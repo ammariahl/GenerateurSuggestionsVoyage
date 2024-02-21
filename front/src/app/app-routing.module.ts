80793044d46ae47116d9a1d77704114521b2f343
AzraeLandres <azraellandres@gmail.com>
2024-02-21 14:49:03 +0100

    Update CSS and add 404 component

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { HomeComponent } from './home/home.component';
 import { SuggestionComponent } from './suggestion/suggestion.component';
 import { AboutComponent } from './about/about.component';
 import { SearchDestinationComponent } from './search-destination/search-destination.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'random', component: SuggestionComponent },
   { path: 'about', component: AboutComponent },
@@ -15,6 +15,9 @@ const routes: Routes = [
     path: 'api/destinations/top/:periode/:climat/:budget/:activity/:documents',
     component: SuggestionComponent,
   },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/page-not-found' }, // Wildcard route for a 404 page
 ];
 
 @NgModule({
