f8f3021a567f1daf69c85fabb8e0e2beab696b9c
ammariahl <ammariahellou@gmail.com>
2024-02-22 02:05:52 +0100

    update redirect page notfound and home

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,14 +4,15 @@ import { HomeComponent } from './home/home.component';
 import { SuggestionComponent } from './suggestion/suggestion.component';
 import { AboutComponent } from './about/about.component';
 import { SearchDestinationComponent } from './search-destination/search-destination.component';
-import { SearchDestinationComponent } from './search-destination/search-destination.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'random', component: SuggestionComponent },
   { path: 'about', component: AboutComponent },
   { path: 'search', component: SearchDestinationComponent },
+  { path: '**', component: PageNotFoundComponent },
   {
     path: 'api/destinations/top/:periode/:climat/:budget/:activity/:documents',
     component: SuggestionComponent,
