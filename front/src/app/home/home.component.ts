bf6b626a518fbebb0abb98ec4d6c88ee46168e2e
ammariahl <ammariahellou@gmail.com>
2024-02-23 03:23:43 +0100

    Home modification + some correction

--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { SharedDestinationService } from '../TravalService/Shared-destination.service';
+import { DestinationCard } from '../models/destination-card.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  winterDestinations: DestinationCard[] = [];
+  familyDestinations: string[] = ['1', '2', '3'];
+  adventureDestinations: string[] = ['1', '2', '3'];
 
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private sharedDestinationService: SharedDestinationService
+  ) {}
+
+  ngAfterViewInit(): void {
+    // Subscribe to the destinations$ observable when the component initializes
+    this.sharedDestinationService.destinations$.subscribe(
+      (destinations) => {
+        console.log('Received destinations from shared service:', destinations);
+        destinations.forEach((destination) => {
+          const selectedSeasons = destination.seasons;
+          const selectedBudgets = destination.budgets;
+          const selectedActivities = destination.activities;
+          const selectedDocuments = destination.documents;
+          // Accessing seasons
+          console.log(
+            'Seasons for destination',
+            destination.name,
+            ':',
+            selectedSeasons
+          );
+
+          // Accessing budgets
+          console.log(
+            'Budgets for destination',
+            destination.name,
+            ':',
+            selectedBudgets
+          );
+
+          // Accessing activities
+          console.log(
+            'Activities for destination',
+            destination.name,
+            ':',
+            selectedActivities
+          );
+
+          // Accessing documents
+          console.log(
+            'Documents for destination',
+            destination.name,
+            ':',
+            selectedDocuments
+          );
+        });
+        this.winterDestinations = destinations;
+        this.cdr.detectChanges();
+      },
+      (error) => {
+        console.error('Error getting destinations:', error);
+        console.log('No matching destinations:');
+      }
+    );
+  }
+
+  showDestinationCard(destination: DestinationCard): void {}
 }
--- front/src/app/menu/menu.component.ts
+++ front/src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { TravelService } from '../TravalService/travalService';
+import { catchError, of } from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -8,8 +11,72 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
   isMenuOpen = false;
+  budgets: Array<string> = [
+    'littleBudget',
+    'mediumBudget',
+    'bigBudget',
+    'unlimited',
+  ];
+  activities: Array<string> = [
+    'relaxing',
+    'adventure',
+    'groupactivity',
+    'family',
+  ];
+  documents: Array<string> = ['cniUe', 'passportUe', 'visaUe', 'passportMde'];
+
+  constructor(
+    private router: Router,
+    private travelService: TravelService,
+    private zone: NgZone,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  sendSearchDestinationForm(): void {
+    // Selection submitted
+    const preferences = {
+      seasons: [{ ['winter']: 'froid' }],
+      budgets: this.budgets.reduce((acc, doc) => {
+        if (doc.trim() !== '') {
+          acc.push({ [doc.replace(/ /g, '')]: true });
+        }
+        return acc;
+      }, [] as { [key: string]: boolean }[]),
+      documents: this.documents.reduce((acc, doc) => {
+        if (doc.trim() !== '') {
+          acc.push({ [doc.replace(/ /g, '')]: true });
+        }
+        return acc;
+      }, [] as { [key: string]: boolean }[]),
+      activities: this.activities.reduce((acc, doc) => {
+        if (doc.trim() !== '') {
+          acc.push({ [doc.replace(/ /g, '')]: true });
+        }
+        return acc;
+      }, [] as { [key: string]: boolean }[]),
+    };
+
+    const userPreference = JSON.stringify(preferences);
+    // console.log(JSON.stringify(preferences));
+    console.log('User Preference:', userPreference);
+    this.travelService
+      .sendTravelPreferences(userPreference)
+      .pipe(
+        catchError((error) => {
+          console.error('Error sending preferences:', error);
+          // Handle the error or rethrow it as needed
+          return of(null); // or throw error;
+        })
+      )
+      .subscribe((response) => {
+        if (response !== null) {
+          console.log('Response from backend:', response);
+          console.log('Data from response:', response);
+        }
+      });
   }
 }
