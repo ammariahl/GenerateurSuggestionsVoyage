0a2a8fb500d910fe948a4c41ae7c929b66da5dde
AzraeLandres <azraellandres@gmail.com>
2024-02-26 16:08:31 +0100

    Add step values and enable step completion

--- front/src/app/search-destination/search-destination.component.ts
+++ front/src/app/search-destination/search-destination.component.ts
@@ -52,6 +52,8 @@ export class SearchDestinationComponent {
     activity: [],
     documents: [],
   };
+  stepCompleted: { [key: number]: boolean } = { 0: true };
+  stepValues: { [key: number]: string } = {};
 
   // Je crée le formulaire
   preferencesForm = this.formBuilder.group({
@@ -77,6 +79,8 @@ export class SearchDestinationComponent {
   //Je récupère les infos des boutons cliqués
   setPreference(field: string, value: string, step: number): void {
     this.preferencesForm.get(field)?.setValue(value);
+    this.stepValues[step] = value;
+    this.stepCompleted[step + 1] = true;
     this.onButtonClick(step);
     this.cdr.detectChanges();
     console.log('Field:', field, 'Value:', value, 'Step:', step);
